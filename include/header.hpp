a266fdeba67d1e4eac54b8f376931c48197c8e97
VovaGrigoryan <stalker17478@mail.ru>
2020-02-02 18:25:29 +0300

    lab

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,164 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <atomic>
+#include <exception>
+#include <iostream>
+#include <utility>
+
+class Counter {
+private:
+    std::atomic_int counter;
+
+public:
+    Counter() : counter(0){};
+
+    void inc() {
+        counter++;
+    }
+
+    void dec() {
+        counter--;
+    }
+
+    int get() {
+        return counter;
+    }
+
+    ~Counter() = default;
+};
+
+template<typename T>
+class SharedPtr {
+private:
+    T *data;
+    Counter *control;
+
+public:
+    SharedPtr() {
+        data = nullptr;
+        control = nullptr;
+    }
+
+    SharedPtr(const SharedPtr &r) {
+        control = r.control;
+        if (r == true)
+            control->inc();
+
+        data = r.data;
+    }
+
+    SharedPtr(SharedPtr &&r) noexcept {
+        control = r.control;
+        if (r == true)
+            control->inc();
+
+        data = r.data;
+    }
+
+    explicit SharedPtr(T *ptr) {
+        control = new Counter();
+        control->inc();
+        data = ptr;
+    }
+
+    auto operator=(const SharedPtr &r) -> SharedPtr & {
+        if (*this == true) {
+            control->dec();
+            if (control->get() == 0) {
+                delete control;
+                delete data;
+            }
+        }
+        control = r.control;
+        data = r.data;
+        if (*this == true)
+            control->inc();
+
+        return *this;
+    }
+
+    auto operator=(SharedPtr &&r) -> SharedPtr & {
+        if (*this == true) {
+            control->dec();
+            if (control->get() == 0) {
+                delete control;
+                delete data;
+            }
+        }
+        control = r.control;
+        data = r.data;
+        if (*this == true)
+            control->inc();
+
+        return *this;
+    }
+
+    operator bool() const {
+        return control != nullptr;
+    }
+
+    auto operator*() const -> T & {
+        if (*this == false)
+            throw std::invalid_argument("no object");
+
+        return *data;
+    }
+
+    auto operator->() const -> T * {
+        return data;
+    }
+
+    auto get() -> T * {
+        return data;
+    }
+
+    void reset() {
+        if (*this == true) {
+            control->dec();
+            if (control->get() == 0) {
+                delete data;
+                delete control;
+            }
+            data = nullptr;
+            control = nullptr;
+        }
+    }
+
+    void reset(T *ptr) {
+        if (*this == true) {
+            control->dec();
+            if (control->get() == 0) {
+                delete data;
+                delete control;
+            }
+            control = new Counter();
+            control->inc();
+            data = ptr;
+        }
+    }
+
+    void swap(SharedPtr &r) {
+        if (data != r.data)
+            std::swap(r, *this);
+    }
+
+    auto use_count() const -> size_t {
+        if (*this == false)
+            return 0;
+
+        return static_cast<size_t>(control->get());
+    }
+
+    ~SharedPtr() {
+        if (*this == true) {
+            control->dec();
+            if (control->get() == 0) {
+                delete control;
+                delete data;
+            }
+        }
+    }
+};
+
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -2,6 +2,79 @@
 
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include <header.hpp>
+
+TEST(SharedPtr, init) {
+    SharedPtr<int> sp1;
+    EXPECT_EQ(sp1, false);
+
+    SharedPtr<int> sp2(new int(13));
+
+    EXPECT_EQ(sp2, true);
+    EXPECT_EQ(*sp2, 13);
+    EXPECT_EQ(sp2.use_count(), 1);
+
+    SharedPtr<int> sp3(sp2);
+
+    EXPECT_EQ(sp3, true);
+    EXPECT_EQ(*sp2, *sp3);
+    EXPECT_EQ(sp2.use_count(), 2);
+    EXPECT_EQ(sp3.use_count(), 2);
+    EXPECT_EQ(sp2.get(), sp3.get());
+}
+
+TEST(SharedPtr, assign) {
+    SharedPtr<int> sp1(new int(28));
+    auto sp2 = sp1;
+
+    EXPECT_EQ(*sp2, 28);
+    EXPECT_EQ(sp1.use_count(), 2);
+    EXPECT_EQ(sp1.use_count(), sp2.use_count());
+
+    SharedPtr<int> sp3(new int(0));
+
+    EXPECT_EQ(sp3.use_count(), 1);
+
+    sp1 = sp3;
+
+    EXPECT_EQ(sp3.use_count(), 2);
+    EXPECT_EQ(*sp1, 0);
+}
+
+TEST(SharedPtr, reset_swap) {
+    SharedPtr<int> sp1(new int(2));
+
+    EXPECT_EQ(sp1.use_count(), 1);
+
+    sp1.reset();
+
+    EXPECT_EQ(sp1.use_count(), 0);
+    EXPECT_THROW(*sp1, std::invalid_argument);
+
+    SharedPtr<int> sp2(new int(2));
+    sp1 = sp2;
+
+    EXPECT_EQ(sp2.use_count(), 2);
+    EXPECT_EQ(*sp2, 2);
+
+    sp2.reset(new int(2001));
+
+    EXPECT_EQ(sp2.use_count(), 1);
+    EXPECT_EQ(*sp2, 2001);
+    EXPECT_EQ(sp1.use_count(), 1);
+    EXPECT_EQ(*sp1, 2);
+
+    SharedPtr<int> sp3(sp2);
+
+    EXPECT_EQ(sp3.use_count(), 2);
+    EXPECT_EQ(*sp3, 2001);
+    EXPECT_EQ(sp1.use_count(), 1);
+    EXPECT_EQ(*sp1, 2);
+
+    sp3.swap(sp1);
+
+    EXPECT_EQ(sp3.use_count(), 1);
+    EXPECT_EQ(*sp3, 2);
+    EXPECT_EQ(sp1.use_count(), 2);
+    EXPECT_EQ(*sp1, 2001);
 }
